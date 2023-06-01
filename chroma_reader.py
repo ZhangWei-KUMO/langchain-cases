517c40aa0cd4674d50a27902d34faba610b4ffd5
zhangwei-kumo <kanseefoil@gmail.com>
2023-06-01 16:07:26 +0800

    add

--- chroma_reader.py
+++ chroma_reader.py
@@ -4,7 +4,12 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.vectorstores import Chroma,Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
 
+# 读取环境变量
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 # 用于加载pdf文件的方法
 loader = UnstructuredPDFLoader('../book.pdf')
 # 加载数据, 这里的数据就是普遍的文本数据, 其中pdf内容位于data[0].page_content中
@@ -20,8 +25,8 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
 # 将文本拆分为块，注意这里只能对原始data进行拆分，不能对content进行拆分
 texts = text_splitter.split_documents(data)
 print(f'一共被拆分为{len(texts)}块')
-embeddings = OpenAIEmbeddings(openai_api_key="apChPf5QkF7DFMOmndW8T3BlbkFJ8w7c1Q7d7rmt379Pz230")
-# docsearch = Chroma.from_documents(texts, embeddings)
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+docsearch = Chroma.from_documents(texts, embeddings)
 # query = "这本书的内容是啥？"
 # docs = docsearch.similar_documents(query, include_metadata=True)
 # llm = OpenAI(temperature=0,openai_api_key="apChPf5QkF7DFMOmndW8T3BlbkFJ8w7c1Q7d7rmt379Pz230")
--- pinecone_reader.py
+++ pinecone_reader.py
@@ -2,28 +2,39 @@ from langchain.document_loaders import UnstructuredPDFLoader,OnlinePDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
-
-loader = UnstructuredPDFLoader('../book.pdf')
-data = loader.load()
-print(f'文档中{len(data[0].page_content)}字')
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
-texts = text_splitter.split_documents(data)
-print(f'文本{len(texts)}')
-from langchain.vectorstores import Chroma,Pinecone
+import os
+from dotenv import load_dotenv
+from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 import pinecone
 
-embeddings = OpenAIEmbeddings(openai_api_key='')
-pinecone.init(
-    api_key='',
-    environment=''
-)
-index_name="langchain_test"
-docsearch = Pinecone.from_texts([t.page_content for t in texts],embeddings,index_name=index_name)
+load_dotenv('.env')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+EPINECONE_NVIRONMENT = os.getenv('EPINECONE_NVIRONMENT')
+loader = UnstructuredPDFLoader('../Tabnet.pdf')
+data = loader.load()
+print(f'文档中{len(data[0].page_content)}字')
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=0)
+texts = text_splitter.split_documents(data)
+print(f'文本{len(texts)}')
 
-# query = "TabNet算法是什么？"
-llm = OpenAI(temperature=0,openai_api_key='')
+
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment=EPINECONE_NVIRONMENT
+)
+docsearch = Pinecone.from_texts(
+    [t.page_content for t in texts],
+    embeddings,
+    index_name='freshbird')
+
+llm = OpenAI(temperature=0,openai_api_key=OPENAI_API_KEY)
+# 基于OpenAI大语言模型构建问答链，分为stuff,map_reduce,refine,map_rerank四种
 chain = load_qa_chain(llm=llm,chain_type="stuff")
-query = "这本书的内容是啥？"
-docs = docsearch.similar_documents(query, include_metadata=True)
-chain.run(input_document=docs, question=query)
+query = "这篇论文的作者是谁"
+docs = docsearch.similarity_search(query)
+print("---2--")
+result = chain({"input_documents": docs, "question": query}, return_only_outputs=True)
+print(result)

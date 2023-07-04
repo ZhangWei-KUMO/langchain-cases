83f0a66231b62e0c2b01ff0622d538599492fc7f
zhangwei-kumo <kanseefoil@gmail.com>
2023-07-04 22:49:43 +0800

    submit code

--- chains/vector.py
+++ chains/vector.py
@@ -8,7 +8,7 @@ load_dotenv('.env')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-loader = PyPDFLoader('./media.pdf')
+loader = PyPDFLoader('./Psychotherapy.pdf')
 documents = loader.load()
 
 text_splitter = CharacterTextSplitter(chunk_size=1600, chunk_overlap=200)
